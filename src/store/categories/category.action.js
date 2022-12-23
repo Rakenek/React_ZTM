b6e4c6a23c40cc1dc976c2ee2e334cffc01c138c
Rakenek <kamilotawski@gmail.com>
2022-12-23 12:36:14 +0100

    178

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,5 @@
 import { createAction } from '../../utlis/reducer/reducer.utils';
 import CATEGORIES_ACTION_TYPES from './category.types';
-import { getCategoriesAndDocuments } from '../../utlis/firebase/firebase.utils';
 
 export const fetchCategoriesStart = () =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
@@ -12,13 +11,3 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
   );
 export const fetchCategoriesFailed = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-  try {
-    const categoriesArray = await getCategoriesAndDocuments();
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-};
