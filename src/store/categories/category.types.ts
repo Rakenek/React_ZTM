bd97a6a821a4e34183d2adb3cad64a9a472f139f
Rakenek <kamilotawski@gmail.com>
2023-01-19 23:16:27 +0100

    203

--- src/store/categories/category.types.ts
+++ src/store/categories/category.types.ts
@@ -4,4 +4,21 @@ enum CATEGORIES_ACTION_TYPES {
   FETCH_CATEGORIES_FAILED = 'category/FETCH_CATEGORIES_FAILED',
 }
 
+export type CategoryItem = {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+};
+
+export type Category = {
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[];
+};
+
 export default CATEGORIES_ACTION_TYPES;
+
+export type CategoryMap = {
+  [key: string]: CategoryItem[];
+};
